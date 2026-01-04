14b87179655cbe78307364649a91d84d48be39cd
tanstevn <tanstevenlester@gmail.com>
2026-01-04 23:39:27 +0800

    Add skeleton codebase structure

---
+++ Backend/Pakturaly.Api/DependencyInjection.cs
@@ -0,0 +1,71 @@
+﻿using Scalar.AspNetCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Pakturaly.Api {
+    public static class DependencyInjection {
+        public static void ConfigureConfiguration(IConfigurationManager config) {
+            config.AddJsonFile("appsettings.local.josn", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
+
+        public static void ConfigureServices(IServiceCollection services, IConfiguration config) {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            services.AddOpenApi(options => {
+                options.AddDocumentTransformer((document, context, _) => {
+                    document.Info = new() {
+                        Title = "Pakturaly API",
+                        Version = "v1",
+                        Description = "API for Pakturaly application",
+                        Contact = new() { 
+                            Name = "Pakturaly API Support",
+                            Email = "tanstevenlester@gmail.com"
+                        }
+                    };
+
+                    return Task.CompletedTask;
+                });
+            });
+        }
+
+        public static void ConfigureHost(ConfigureHostBuilder host, IConfigurationManager config) {
+
+        }
+
+        [SuppressMessage("Usage", "ASP0014:Suggest using top level route registrations", Justification = "")]
+        public static void ConfigureApplication(WebApplication app, IWebHostEnvironment env) {
+            if (!env.IsEnvironment("PROD")) {
+                app.MapOpenApi().CacheOutput();
+                app.MapScalarApiReference();
+
+                app.MapGet("/", () => Results.Redirect("/scalar/v1"))
+                    .ExcludeFromDescription();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapFallback(context => {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+                    return context
+                        .Response
+                        .WriteAsync(string.Empty);
+                });
+            });
+
+            // Middlewares here
+
+            InitializeRequiredServices(app);
+        }
+
+        private static void InitializeRequiredServices(IApplicationBuilder app) {
+
+        }
+    }
+}
---
+++ Backend/Pakturaly.Api/Program.cs
@@ -0,0 +1,20 @@
+using Pakturaly.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+DependencyInjection.ConfigureConfiguration(builder.Configuration);
+DependencyInjection.ConfigureHost(builder.Host, builder.Configuration);
+DependencyInjection.ConfigureServices(builder.Services, builder.Configuration);
+
+var app = builder.Build();
+DependencyInjection.ConfigureApplication(app, app.Environment);
+
+//app.MapGet("/products", (int? pageSize, int? page) => {
+//    return Results.Ok(new());
+//})
+//    .Produces(400)
+//    .WithName("GetProducts")
+//    .WithTags("Products")
+//    .WithSummary("Retrieve a list of products");
+
+app.Run();
