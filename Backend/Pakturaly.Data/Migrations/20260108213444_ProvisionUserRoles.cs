58739d97898af24ff326c894278c788a1cb789c7
tanstevn <tanstevenlester@gmail.com>
2026-01-09 05:51:28 +0800

    Add provisioning of user roles

---
+++ Backend/Pakturaly.Data/Migrations/20260108213444_ProvisionUserRoles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pakturaly.Data.Migrations {
+    /// <inheritdoc />
+    public partial class ProvisionUserRoles : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[,] {
+                    { Guid.NewGuid().ToString(), "Admin", "ADMIN", Guid.NewGuid().ToString() },
+                    { Guid.NewGuid().ToString(), "Member", "MEMBER", Guid.NewGuid().ToString() }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Name",
+                keyValues: new object[] { "Admin", "Member"});
+        }
+    }
+}
